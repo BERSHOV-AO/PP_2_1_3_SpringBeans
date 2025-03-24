5a99b44498d5fd8f73cfaac4cbc843e4cf7dfc68
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-03-24 13:43:39 +0300

    Add Dog. Refactor AnimalsCage, Timer, Application

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,6 +16,9 @@ public class Application {
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
-    }
 
+        Timer tm1 = applicationContext.getBean(Timer.class);
+        Timer tm2 = applicationContext.getBean(Timer.class);
+        System.out.println("Сравнение: " + (tm1.getTime() == tm2.getTime()));
+    }
 }
